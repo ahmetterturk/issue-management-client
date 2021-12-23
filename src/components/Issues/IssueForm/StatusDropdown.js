95af91699eec4e72408d952132dfb4e1cd9f78e3
ahmetterturk <ahm.ert@hotmail.com>
2021-12-23 14:38:31 +1100

    change file structure in issues page and issue form, removed unnecessary files and style files, also fixed a warning where it said to use className instead of class in the sidebar component

--- src/components/Issues/IssueForm/StatusDropdown.js
+++ src/components/Issues/IssueForm/StatusDropdown.js
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-const PriorityDropdown = ({ register }) => {
+const StatusDropdown = ({ register }) => {
   return (
     <Box sx={{ minWidth: 200 }}>
       <FormControl sx={{ minWidth: 150 }}>
-        <InputLabel id="demo-simple-select-label">Priority</InputLabel>
+        <InputLabel id="demo-simple-select-label">Status</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           label="Status"
-          {...register('priority', { required: true })}
+          {...register('status', { required: true })}
         >
-          <MenuItem value="Low">Low</MenuItem>
-          <MenuItem value="High">High</MenuItem>
+          <MenuItem value="New">New</MenuItem>
+          <MenuItem value="Pending">Pending</MenuItem>
+          <MenuItem value="Resolved">Resolved</MenuItem>
         </Select>
       </FormControl>
     </Box>
   );
 };
 
-export default PriorityDropdown;
+export default StatusDropdown;
