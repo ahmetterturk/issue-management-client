f82f295a1366126e5b6f34fc604d3705791be0f4
ahmetterturk <ahm.ert@hotmail.com>
2021-12-29 16:13:04 +1100

    create adding messages functionality to issues

---
+++ src/apiServices/MessageApi.js
@@ -0,0 +1,19 @@
+import API from './api';
+
+export const getAllMessages = async () => {
+  try {
+    const response = await API.get('/messages');
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createMessage = async (messageData) => {
+  try {
+    const response = await API.post('/messages', messageData);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
