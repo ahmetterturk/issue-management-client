019e2bc79011e9e39be434c3a5aff069ce12e9e2
ahmetterturk <ahm.ert@hotmail.com>
2022-01-05 17:13:29 +1100

    style issue edit form in a mobile responsive way

--- src/components/IssuePage/styles.js
+++ src/components/IssuePage/styles.js
@@ -1,6 +1,70 @@
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles((theme) => ({
+  boxContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'space-between',
+    backgroundColor: 'white',
+    position: 'absolute',
+    width: 500,
+    height: 600,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    padding: theme.spacing(10),
+    borderRadius: theme.spacing(1),
+    [theme.breakpoints.down('sm')]: {
+      width: 250,
+      height: 550,
+      padding: theme.spacing(7),
+    },
+  },
+  dropdownContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(4),
+    },
+  },
+  select: {
+    minWidth: 280,
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      marginBottom: theme.spacing(4),
+    },
+    [theme.breakpoints.down('sm')]: {
+      minWidth: 316,
+    },
+  },
+  personSelect: {
+    minWidth: 570,
+    maxWidth: 570,
+    [theme.breakpoints.down('sm')]: {
+      minWidth: 316,
+      maxWidth: 316,
+    },
+  },
+  gridItem: {
+    margin: theme.spacing(3),
+  },
+  titleDiv: {
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(4),
+    },
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(2),
+    },
+  },
+  descriptionDiv: {
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(4),
+      marginBottom: theme.spacing(4),
+    },
+    marginBottom: theme.spacing(2),
+  },
   container: {
     marginTop: '50px',
   },
