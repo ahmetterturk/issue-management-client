610af1ba184c94535b19cf5e495a53f998164ae0
ahmetterturk <ahm.ert@hotmail.com>
2022-01-08 12:18:41 +1100

    change the employee to see his private issues aswell

--- src/apiServices/api.js
+++ src/apiServices/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://issue-management-backend.herokuapp.com',
+  // baseURL: 'http://localhost:5000',
 });
 
 API.interceptors.request.use((req) => {
