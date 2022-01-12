2f19dccd46910b68135e3dda1df71626c4d9f604
arturo-caceres <arturo.caceres02383@gmail.com>
2022-01-12 20:41:39 +1100

    Fixing tests

--- src/components/Login/LoginForm.test.js
+++ src/components/Login/LoginForm.test.js
@@ -11,9 +11,6 @@ import { act } from '@testing-library/react/dist/pure';
 
 const LoginWithProviders = WithProviders(LoginFormView);
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJpc0FkbWluIjp0cnVlfQ.5T-y5t-zMXrVQFZ62AmTu0zGAzKYbqfkh1U02USuDZY';
-
 describe('Login', () => {
   it("should render app's name", () => {
     render(<LoginWithProviders />);
