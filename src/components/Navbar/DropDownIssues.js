d7eb2bd56a15e07c7d25d2f8540b0e49a08910a2
saman-zdf <samanfyz68@gmail.com>
2022-01-13 08:39:14 +0800

    added comments to the Navbar, DropDownIssues and singleDropDownIssue

--- src/components/Navbar/DropDownIssues.js
+++ src/components/Navbar/DropDownIssues.js
@@ -12,25 +12,36 @@ import { Typography, Divider } from '@mui/material';
 const DropDownIssues = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  // getting state and disptahc from globalState
   const { state, dispatch } = useGlobalContext();
-  const { userDetails } = state.currentUser;
-  const { issues, assignedIssues } = state;
+  // destructuring issues, assignedIssues and currentUser from state
+  const { issues, assignedIssues, currentUser } = state;
+  // destructuring userDetails from current user
+  const { userDetails } = currentUser;
 
+  // usign useEffect to create notification on navbar
   React.useEffect(() => {
-    if (state.currentUser) {
+    // first check if the currentUser exist
+    if (currentUser) {
+      // declaring new variable to create a new array by maping and filtering issues state
       const currentAssignedIssues = issues
         .map((issue) => {
+          // issue model has members property, first check if memebrs includes userDetails firstName and lastName,
           if (
             issue.members.includes(
               `${userDetails.firstName} ${userDetails.lastName}`
             )
           ) {
+            // if it does will return the condioned issue
             return issue;
           }
+          // if it doesn't it will return the rest of issues as undefine, here will filter those are undefined
         })
         .filter((issue) => issue !== undefined);
+      // using dispatch to set the currentAssignedIssues to the assignedIssue state in global state
       dispatch({ type: 'SET_ASSIGNED_ISSUES', data: currentAssignedIssues });
     }
+    // using currentUser, issues, counter as a dependency to rerender the current useEffect if any of these state has new data
   }, [state.currentUser, state.issues, state.counter, dispatch]);
 
   const handleClick = (event) => {
@@ -49,6 +60,7 @@ const DropDownIssues = () => {
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
       >
+        {/* badge content will show the length of assignedIssues as a number of notification */}
         <Badge badgeContent={assignedIssues.length} color='secondary'>
           <NotificationsIcon color='action' />
         </Badge>
@@ -64,7 +76,9 @@ const DropDownIssues = () => {
         }}
       >
         <Divider />
+        {/* here we check if assignedIssues state is exist and if the keys length bigger then 0 */}
         {assignedIssues && Object.keys(assignedIssues).length > 0 ? (
+          // mapping through assignedIssues to show them in dropdown menu with link from react router dom to navigate to specific issues
           assignedIssues.map((issue) => {
             return (
               <Link
@@ -75,6 +89,7 @@ const DropDownIssues = () => {
                   color: '#555',
                 }}
               >
+                {/* in singleDropDown components we will show the issues to the currentUser if user has been assign to any */}
                 <SingleDropdownIssue handleClose={handleClose} issue={issue} />
               </Link>
             );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,12 +11,16 @@ import DropDownIssues from './DropDownIssues';
 
 const Navbar = (props) => {
   const { onMenuClick } = props;
+  // isLarge screent will check if them breakuppoints is in lg size if it is set the properties to false
   const isLargeScreen = useMediaQuery((theme) => theme.breakpoints.up('lg'), {
     defaultMatches: true,
     noSsr: false,
   });
+  // using classes from useStyle that we made with makeStyles from mui
   const classes = useStyles({ isLargeScreen });
+  // getting state from globalcontext
   const { state } = useGlobalContext();
+  // destructuring the userDetails from state currentUser
   const { userDetails } = state.currentUser;
 
   const menuIcon = (
@@ -39,6 +43,7 @@ const Navbar = (props) => {
       <Toolbar className={classes.navbar}>
         {!isLargeScreen && menuIcon}
         <Box className={classes.avatarWrapper}>
+          {/* rendering the dropDown issues in AppBar */}
           <DropDownIssues />
           <span className={classes.userGreeting}>
             Hi{' '}
